fc3572e10c828193d1f5fe4bac1a5f994b38e6d6
Richard T <74796181+rtillies@users.noreply.github.com>
2024-10-23 11:06:32 -0400

    Refactor methods

--- CarTester.java
+++ CarTester.java
@@ -1,27 +1,49 @@
 
 public class CarTester {
+  // public static void main(String[] args) throws Exception {
   public static void main(String[] args) {
     Car lexus = new Car("Lexus ES350", 48000, 26, 18875, 24500);
     Car tucson = new Car("2014 Hyundai Tucson", 12500, 32, 9850, 17500);
+
+    // Try to change existing info
+    lexus.setID("New Lexus");
+    lexus.setMileage(10000);
 
     // Sell Lexus
     lexus.sellCar(25000);
 
     // Print car information
-    System.out.println(lexus);
-    System.out.println(tucson);
+    // System.out.println(lexus);
+    // System.out.println(tucson);
 
     // Better Mileage?
     System.out.print("Better Mileage: ");
-    System.out.println((lexus.compareMPG(tucson) > 0) ? lexus.getID() : tucson.getID());
-
+    System.out.println(betterMileage(lexus, tucson).getID());
+    
+    
     // More Miles?
     System.out.print("More Miles:     ");
-    System.out.println((lexus.compareMiles(tucson) > 0) ? lexus.getID() : tucson.getID());
-
+    System.out.println(moreMiles(lexus, tucson).getID());
+    
     // Higher Price?
     System.out.print("Higher Price:   ");
-    System.out.println((lexus.comparePrice(tucson) > 0) ? lexus.getID() : tucson.getID());
-
+    System.out.println(higherPrice(lexus, tucson).getID());
   }
+
+  // Return car with better mileage
+  public static Car betterMileage(Car a, Car b) {
+    return a.compareMPG(b) > 0 ? a : b;
+  }
+
+  // Return car with more miles
+  public static Car moreMiles(Car a, Car b) {
+    return a.compareMiles(b) > 0 ? a : b;
+  }
+
+  // Return car with higher price
+  public static Car higherPrice(Car a, Car b) {
+    return a.comparePrice(b) > 0 ? a : b;
+  }
+
+  
 }
